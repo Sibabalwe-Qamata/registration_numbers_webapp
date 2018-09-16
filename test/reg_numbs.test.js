32fd59094f699cdb9db06b3c34690e00dd56e28f
Sibabalwe Qamata <prof.qamata@gmail.com>
2018-09-16 13:52:32 +0200

    updated the test cases

--- test/reg_numbs.test.js
+++ test/reg_numbs.test.js
@@ -26,10 +26,34 @@ describe("The Registration Numbers WebApp Database Unit Tests", async function()
     assert.equal(await reg.validateInput("CA"),1);
   });
 
-  // it("It should return the town ID of the town entered.", async function() 
-  // {
-  //   //let reg = await regNumFactory(pool);
-  //   //assert.equal(await reg.validateInput("CEO 1485"), 21);
-  // });
+  it("It should return the registration number of the town entered.", async function() 
+  {
+    let reg = await regNumFactory(pool);
+
+    assert.equal(await reg.enterRegPlate("CJ 5246", "CJ"),"CJ 5246");
+  });
+
+
+  it("It should return all the registration numbers in the database.", async function() 
+  {
+    let reg = await regNumFactory(pool);
+    await reg.enterRegPlate("CJ 5246", "CJ");
+    await reg.enterRegPlate("CW 5246", "CW");
+    await reg.enterRegPlate("CA 5246", "CA");
+    assert.equal(await reg.getPlate(),"CJ 5246");
+  });
+
+
+  it("It should return all the registration numbers as per the selected town.", async function() 
+  {
+    let reg = await regNumFactory(pool);
+    await reg.enterRegPlate("CJ 5246", "CJ");
+    await reg.enterRegPlate("CJ 0146", "CJ");
+  
+    await reg.dropDown("CJ");
+    assert.equal(await reg.filterTown("CJ"),["CJ 5246", "CJ 0146"]);
+  });
+
+
 
 });
